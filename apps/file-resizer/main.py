a2d352ef4379738625bfb793d692d8ab59fb5eb6
Marco Plaisier <m.plaisier@gmail.com>
2025-09-22 21:23:41 +0200

    Small service to be deployed to K3S

---
+++ apps/file-resizer/main.py
@@ -0,0 +1,40 @@
+import os
+
+from PIL import Image
+from flask import Flask, render_template, request, send_from_directory
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/images/<filename>')
+def images(filename):
+    return send_from_directory('images', filename)
+
+
+@app.route('/output/<filename>')
+def output(filename):
+    return send_from_directory('output', filename)
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files['image']
+    filename = secure_filename(file.filename)
+    saved_file_name = os.path.join(os.getcwd(), "images", filename)
+    file.save(saved_file_name)
+    img = Image.open(saved_file_name)
+    img = img.resize((300, int(img.height * 300 / img.width)))
+    resized_file = os.path.join(os.getcwd(), "output", filename)
+    img.save(resized_file)
+    return render_template("images.html",
+                           filename=filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
